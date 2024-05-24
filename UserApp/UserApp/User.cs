153e87232ea2d54ae6db218ba3dff05241965eda
svetyxley <svetyx@gmail.com>
2024-05-24 08:47:59 +0300

    Separate input  to class

--- UserApp/UserApp/User.cs
+++ UserApp/UserApp/User.cs
@@ -5,7 +5,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public readonly int Age;
-        Gender Gender { get; set; }
+        public Gender Gender { get; set; }
 
         //Конструктор без параметрів
         //В конструкторі дефолтні значення
@@ -24,60 +24,6 @@
             LastName = "";
             Age = age;
             Gender = Gender.Unknown;
-        }
-
-        public void InputUserName(InputController inputController)
-        {
-            //Input First Name
-            do
-            {
-                Console.Write("Enter First Name: ");
-                FirstName = Console.ReadLine();
-                if (!inputController.IsValidName(FirstName))
-                {
-                    Console.WriteLine("Invalid input. Please enter a valid first name.");
-                    Console.WriteLine();
-                }
-            } while (!inputController.IsValidName(FirstName));
-
-
-            //Input Last Name
-            do
-            {
-                Console.Write("Enter Last Name: ");
-                LastName = Console.ReadLine();
-                if (!inputController.IsValidName(LastName))
-                {
-                    Console.WriteLine("Invalid input. Please enter a valid last name.");
-                    Console.WriteLine();
-                }
-            } while (!inputController.IsValidName(LastName));
-        }
-
-        //Input Gender (5 attempts for invalide input)
-        public void InputUserGender()
-        {
-            for (int i = 0; i <= 4; i++)
-            {
-                Console.Write("Enter gender (Male, Female, Unknown): ");
-                try
-                {
-                    Gender = (Gender)Enum.Parse(typeof(Gender), Console.ReadLine(), ignoreCase: true);
-                    i = 4;
-                }
-                catch (ArgumentException)
-                {
-                    if (i < 4) //outpot ma
-                    {
-                        Console.WriteLine("Invalid input. Please enter a valid gender.");
-                        Console.WriteLine();
-                    }
-                    else
-                    {
-                        Console.WriteLine("You have used five attempts and entered incorrect data. Your Gender will be set up а default value: Unknown");
-                    }
-                }
-            }
         }
 
         public override string ToString()
---
+++ UserApp/UserApp/UserController.cs
@@ -0,0 +1,59 @@
+﻿namespace UserApp
+{
+    internal class UserController
+    {
+        public void InputUserName(InputController inputController, User user)
+        {
+            //Input First Name
+            do
+            {
+                Console.Write("Enter First Name: ");
+                user.FirstName = Console.ReadLine();
+                if (!inputController.IsValidName(user.FirstName))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid first name.");
+                    Console.WriteLine();
+                }
+            } while (!inputController.IsValidName(user.FirstName));
+
+
+            //Input Last Name
+            do
+            {
+                Console.Write("Enter Last Name: ");
+                user.LastName = Console.ReadLine();
+                if (!inputController.IsValidName(user.LastName))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid last name.");
+                    Console.WriteLine();
+                }
+            } while (!inputController.IsValidName(user.LastName));
+        }
+
+        //Input Gender (5 attempts for invalide input)
+        public void InputUserGender(User user)
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.Write("Enter gender (Male, Female, Unknown): ");
+                try
+                {
+                    user.Gender = (Gender)Enum.Parse(typeof(Gender), Console.ReadLine(), ignoreCase: true);
+                    i = 4;
+                }
+                catch (ArgumentException)
+                {
+                    if (i < 4) //outpot ma
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid gender.");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You have used five attempts and entered incorrect data. Your Gender will be set up а default value: Unknown");
+                    }
+                }
+            }
+        }
+    }
+}
